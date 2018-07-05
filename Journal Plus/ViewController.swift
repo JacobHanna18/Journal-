9aa0bbca65011fdd7630a2c69afd95f6522adff0
Jacob Hanna <jacob18111998@gmail.com>
2018-07-05 19:16:59 +0300

    Initial Commit

---
+++ Journal Plus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal Plus
+//
+//  Created by Jacob Hanna on 05/07/2018.
+//  Copyright © 2018 Jacob Hanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
